fcb1bf8c9fab680c00ea7a6a169c0f476eec8ad4
jonathanBensadoun <jonathan.ben-sadoun@oclock.school>
2024-06-01 14:33:32 +0200

    commit adding weather icons and start of image sorting

---
+++ public/icon meteo/icons.md
@@ -0,0 +1,3 @@
+https://www.iconfinder.com/search?q=weather
+https://www.nodc.noaa.gov/archive/arc0021/0002199/1.1/data/0-data/HTML/WMO-CODE/WMO4677.HTM
+https://open-meteo.com/en/docs/meteofrance-api#current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,rain,showers,snowfall,weather_code,cloud_cover,pressure_msl,surface_pressure,wind_speed_10m,wind_direction_10m,wind_gusts_10m
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import './App.scss';
+import Meteo from '../Meteo/Meteo';
 
 function App() {
   const [latitude, setLatitude] = useState(null);
   const [longitude, setLongitude] = useState(null);
+  const [cityName, setCityName] = useState(null);
+  const [dataDay, setDataDay] = useState(null);
 
   useEffect(() => {
     if ("geolocation" in navigator) {
@@ -25,9 +28,34 @@ function App() {
     }
   }, []); 
 
+  // useEffect(() => {
+  //   async function fetchDataWeek() {
+  //       if (latitude !== null && longitude !== null) {
+  //     fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=weather_code,temperature_2m_max,temperature_2m_min,sunrise,sunset,daylight_duration,sunshine_duration,uv_index_max,precipitation_hours,wind_speed_10m_max,wind_gusts_10m_max&timezone=Europe%2FLondon`, {
+  //       method: 'GET',
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //       },
+  //     })
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         if (data) {
+  //         console.log("semaine",JSON.stringify(data));
+  //         }
+  //       })
+  //       .catch((error) => {
+  //         console.error('Error:', error);
+  //       });
+  //     }
+  //     fetchDataWeek();
+  //   }
+  // }, [latitude, longitude]);
+
   useEffect(() => {
     if (latitude !== null && longitude !== null) {
-      fetch(`https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,rain,showers,snowfall,weather_code,cloud_cover,pressure_msl,wind_speed_10m,wind_direction_10m,wind_gusts_10m&timezone=Europe%2FLondon&forecast_days=1`, {
+    async function fetchDataDay() {
+      console.log(latitude, longitude); 
+      fetch(`https://api.open-meteo.com/v1/meteofrance?latitude=${latitude}&longitude=${longitude}&current=temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,rain,showers,snowfall,weather_code,cloud_cover,pressure_msl,surface_pressure,wind_speed_10m,wind_direction_10m,wind_gusts_10m&hourly=temperature_2m`, {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
@@ -35,15 +63,46 @@ function App() {
       })
         .then((response) => response.json())
         .then((data) => {
-          console.log(data);
+          if (data) {
+          console.log("jour",(data));
+          console.log("jour",(data.current));
+          setDataDay(data);
+          }
         })
         .catch((error) => {
           console.error('Error:', error);
         });
+      }
+      fetchDataDay();
     }
   }, [latitude, longitude]);
 
-  return <div className="container">Hello World</div>;
+  useEffect(() => {
+    if (latitude !== null && longitude !== null) {
+    async function fetchDataCity() {
+      await fetch(`https://nominatim.openstreetmap.org/reverse?format=jsonv2&lat=${latitude+1}&lon=${longitude+1}`)
+        .then((response) => response.json())
+        .then((data) => {
+          if (data) {
+          console.log('where',(data));
+          
+          setCityName(data.address);
+          }
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+        });
+      }
+      fetchDataCity();
+    }
+  }, [latitude, longitude]);
+
+  return (
+    <div className="App">
+      
+      <Meteo dataDay={dataDay} cityName={cityName}  />
+    </div>
+  );
 }
 
 export default App;
